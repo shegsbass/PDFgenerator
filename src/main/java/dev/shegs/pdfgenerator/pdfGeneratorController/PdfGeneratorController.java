0ec7a3fb4ce663badda40cc3e83a095bf9951939
Oladokun oluwasegun <oladokunoluwasegun@gmail.com>
2023-01-31 11:15:47 +0100

    Controller class added

---
+++ src/main/java/dev/shegs/pdfgenerator/pdfGeneratorController/PdfGeneratorController.java
@@ -0,0 +1,38 @@
+package dev.shegs.pdfgenerator.pdfGeneratorController;
+
+import dev.shegs.pdfgenerator.pdfGeneratorService.PdfGeneratorService;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Controller
+public class PdfGeneratorController {
+
+    private final PdfGeneratorService pdfGeneratorService;
+
+    public PdfGeneratorController(PdfGeneratorService pdfGeneratorService) {
+        this.pdfGeneratorService = pdfGeneratorService;
+    }
+
+    @GetMapping("/pdf/generate")
+    public void generatePDF(HttpServletResponse response) throws IOException {
+        response.setContentType("application/pdf");
+        DateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd:hh:mm:ss");
+        String currentDateTime = dateFormatter.format(new Date());
+
+
+        String headerKey = "Content-Disposition";
+        String headerValue = "attachment; filename=pdf_" + currentDateTime + ".pdf";
+        response.setHeader(headerKey, headerValue);
+
+        this.pdfGeneratorService.export(response);
+
+    }
+
+}
