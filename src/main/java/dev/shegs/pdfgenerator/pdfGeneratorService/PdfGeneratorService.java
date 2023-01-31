5a8568184ebed01529c8717177d58c0651541ef2
Oladokun oluwasegun <oladokunoluwasegun@gmail.com>
2023-01-31 09:20:26 +0100

    Service class updated

--- src/main/java/dev/shegs/pdfgenerator/pdfGeneratorService/PdfGeneratorService.java
+++ src/main/java/dev/shegs/pdfgenerator/pdfGeneratorService/PdfGeneratorService.java
@@ -1,14 +1,37 @@
 package dev.shegs.pdfgenerator.pdfGeneratorService;
 
-import com.lowagie.text.Document;
+import com.lowagie.text.*;
+import com.lowagie.text.pdf.PdfWriter;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Service;
+
+import java.io.IOException;
 
 @Service
 public class PdfGeneratorService {
 
-    public void export(HttpServletResponse response){
-        Document document = new Document();
+    public void export(HttpServletResponse response) throws IOException {
+        Document document = new Document(PageSize.A4);
+        PdfWriter.getInstance(document, response.getOutputStream());
+
+        document.open();
+        Font fontTitle = FontFactory.getFont(FontFactory.HELVETICA_BOLD);
+        fontTitle.setSize(18);
+
+        Paragraph paragraph = new Paragraph("This is the title", fontTitle);
+        paragraph.setAlignment(paragraph.ALIGN_CENTER);
+
+
+        Font fontParagraph = FontFactory.getFont(FontFactory.TIMES);
+        fontParagraph.setSize(12);
+
+        Paragraph bodyParagraph = new Paragraph("This is the body paragraph of the pdf which is set to a font size of 12 and a font type of TIMES", fontParagraph);
+        bodyParagraph.setAlignment(paragraph.ALIGN_LEFT);
+
+        document.add(paragraph);
+        document.add(bodyParagraph);
+        document.close();
+
     }
 
 }
